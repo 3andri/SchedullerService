42eb7493aef37e99766949478f3c76bffebc8924
tri Andri <triandripp@gmail.com>
2022-07-12 12:00:23 +0700

    -remove setting "--blink-settings=imagesEnabled=false"

--- src/main/java/com/andri/service/schedule/Stock/ChromeDrivergrab.java
+++ src/main/java/com/andri/service/schedule/Stock/ChromeDrivergrab.java
@@ -45,7 +45,7 @@ public class ChromeDrivergrab {
 		System.out.println(configProp.getProperty("andri.chrome.path"));
 		System.setProperty("webdriver.chrome.driver", configProp.getProperty("andri.chrome.path"));
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless", "--blink-settings=imagesEnabled=false");
+		options.addArguments("--headless");
 		options.addArguments("--disable-dev-shm-usage");
 		options.addArguments(
 				"user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36");
