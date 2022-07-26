9e699cca681f857bab8df50ad34dac8714c755c2
tri Andri <triandripp@gmail.com>
2022-07-26 23:44:43 +0700

    -add gainer or losser

--- src/main/java/com/andri/service/schedule/Controller/stock.java
+++ src/main/java/com/andri/service/schedule/Controller/stock.java
@@ -6,29 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andri.service.schedule.Stock.Repo.StockListRepo;
 import com.andri.service.schedule.Stock.Repo.StockRepo;
+import com.andri.service.schedule.Stock.Repo.stockdifferentRepo;
 import com.andri.service.schedule.Stock.model.StockList;
-
+import com.andri.service.schedule.Stock.model.stockdifferent;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/stock")
 public class stock {
 	@Autowired
-	StockListRepo repo;
-	
-	
+	StockListRepo repostock;
+
+	@Autowired
+	stockdifferentRepo repoRank;
+
 	@RequestMapping("/stock-list")
 	public List<StockList> getStockList() {
-		return repo.getStockList();
+		return repostock.getStockList();
 	}
-	
+
 	@RequestMapping("/stock-rank")
-	public List<StockList> getRankList() {
-		return repo.getStockList();
+	public List<stockdifferent> getRankList(@RequestParam(name = "date", required = true) String date,
+			@RequestParam(name = "sort", required = false) String sort) {
+		return repoRank.GetlistRankASC(date);
 	}
 
 }
--- src/main/java/com/andri/service/schedule/Stock/Repo/stockdifferentRepo.java
+++ src/main/java/com/andri/service/schedule/Stock/Repo/stockdifferentRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.andri.service.schedule.Stock.model.StockDate;
@@ -15,6 +16,12 @@ public interface stockdifferentRepo extends JpaRepository<stockdifferent, stockd
 	
 	@Query(value = "SELECT StockCode  FROM stock group by StockCode ",nativeQuery = true)
 	public List<String> GetlistStock();
+	
+	@Query(value = "SELECT * from stockdata.stockdifferent s where `date` =:date ORDER BY percentage ASC limit 50 ",nativeQuery = true)
+	public List<stockdifferent> GetlistRankASC(@Param("date") String date);
+	
+	@Query(value = "SELECT * from stockdata.stockdifferent s where `date` =:date ORDER BY percentage DESC limit 50 ",nativeQuery = true)
+	public List<stockdifferent> GetlistRankDESC(@Param("date") String date);
 	
 	
 
